06515e6c38ced206cf13546af0308d4509bb098b
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2024-12-02 23:17:05 +0700

    feat: fixing finalize getAllWithLimit function

--- php/App/Models/CommentModel.php
+++ php/App/Models/CommentModel.php
@@ -5,7 +5,23 @@ namespace App\Models;
 use App\Models\BaseModel;
 
 class CommentModel extends BaseModel{
-    public static function getAllWithLimit($start, $limit) {
-        return "testing";
+
+    function __construct(string $username, string $password, string $db, string $host = 'localhost')
+    {
+        parent::__construct($username, $password, $host, $db);
+    }
+    public static function getAllWithLimit($table, $select, $limit) {
+        $query = "ORDER BY id DESC LIMIT " . $limit;
+        $result = self::$conn->query("SELECT {$select} FROM {$table} {$query}");
+        "SELECT * FROM table ORDER BY id DESC LIMIT 5";
+        if (!$result) {
+            echo json_encode(['success' => false, 'code' => 500, 'body' => ["message" => "Failed to retrieve data from database"]]);
+        } else {
+            $rows = [];
+            while ($row = $result->fetch_assoc()) {
+                $rows[] = $row["comment"];
+            };
+        };
+        return $rows;
     }
 }
