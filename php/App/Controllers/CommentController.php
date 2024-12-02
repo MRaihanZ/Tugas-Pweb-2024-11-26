ccd0ca6c5caf1a148cf8ffba2271ca3cc877bc2f
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2024-12-02 23:14:43 +0700

    feat: change error message from echo string to ech o and echo result json

--- php/App/Controllers/CommentController.php
+++ php/App/Controllers/CommentController.php
@@ -7,10 +7,14 @@ use App\Services\CommentService;
 class CommentController {
     public static function getComments($data) {
         if (!isset($data)) {
-            echo json_encode(['success' => false, 'message' => 'Data is not given in URI']);
+            echo json_encode(['success' => false, 'code' => 500,'message' => 'Data is not given in URI']);
             return;
         }
         $comments = CommentService::getCommentWithLimit($data);
-        echo json_encode(['success' => true, 'message' => $comments]);
+        $body = [];
+        foreach ($comments as $index => $value) {
+            $body[] = ["comment"=> $comments[$index]];
+        };
+        echo json_encode(['success' => true, 'code' => 200, 'body' => $body]);
     }
 }
