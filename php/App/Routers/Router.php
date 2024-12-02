134112d16d3b08689b023fcad4083942a08639dd
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2024-12-02 23:17:45 +0700

    feat: change error message from echo string to echo json

--- php/App/Routers/Router.php
+++ php/App/Routers/Router.php
@@ -14,16 +14,9 @@ class Router
         // $checkUri = explode("/", explode("{", $uri)[0]);
         // $input = preg_match('/^\/api\/v1\/comments\/(\d+)\/(\d+)$/', $uri, $matches);
         $input = preg_match_all('/\{([^}]+)\}/', $uri, $matches);
-        print_r("<br/>");
-        print_r($input);
-        print_r("<br/>");
         if ($input < 0) {
-            print_r("False");
-            print_r("<br/>");
             self::$routes[] = compact('method', 'uri', 'controller', 'controllerFunction');
         } else {
-            print_r(value: "True");
-            print_r("<br/>");
             $data = $matches[1];
             self::$routes[] = compact('method', 'uri', 'data', 'controller', 'controllerFunction');
         }
@@ -37,38 +30,20 @@ class Router
     public static function dispatch($uri, $method,)
     {
         foreach (self::$routes as $route) {
-            print_r("Before Method");
-            print_r("<br/>");
             if ($method === $route['method']) {
-                print_r("Method True");
-                print_r("<br/>");
-                print_r($uri);
-                print_r("<br/>");
-                print_r(urldecode($uri));
-                print_r("<br/>");
                 $uri = urldecode($uri);
                 $input = preg_match_all('/\{([^}]+)\}/', $uri, $matches);
                 if ($input > 0) {
-                    print_r("TRUE FROM REGEX");
-                    print_r("<br/>");
                     $data = $route["data"];
                     foreach($matches[1] as $index => $value) {
-                        $newUri = str_replace(("{".$data[1][$index]."}"), $matches[1][$index], $uri);
+                        $newUri = str_replace("{".$data[$index]."}", $matches[1][$index], $uri);
                     }
-                    print_r($route["uri"]);
-                    print_r("<br/>");
-                    print_r($newUri);
-                    print_r("<br/>");
                 }
                 if($newUri === $uri) {
-                    print_r("URI True");
-                    print_r("<br/>");
-                    print_r($route['controllerFunction']);
-                    print_r("<br/>");
                     $controller = $route['controller'];
                     $controllerFunction = $route['controllerFunction'];
                     if(isset($route["data"])) {
-                        return $controller::$controllerFunction($route['data']);
+                        return $controller::$controllerFunction($matches[1]);
                     } else {
                         return $controller::$controllerFunction();
                     }
@@ -76,6 +51,6 @@ class Router
                 
             }
         }
-        echo json_encode(['success' => false, 'message' => 'Route not found']);
+        echo json_encode(['success' => false, 'code' => 500, 'message' => 'Route not found']);
     }
 }
