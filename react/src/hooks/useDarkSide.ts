cff6be8606caf804b4d0e383f6f49e77fca9b35b
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2024-12-03 16:47:56 +0700

    feat: cleaning the code

--- react/src/hooks/useDarkSide.ts
+++ react/src/hooks/useDarkSide.ts
@@ -1,24 +1,15 @@
-// Filename - hooks/useDarkSide.ts
-
 import { useState, useEffect } from "react";
 
-// Type for the theme could be 'light' | 'dark'
 type Theme = 'light' | 'dark';
 
 export default function useDarkSide(): [Theme, React.Dispatch<React.SetStateAction<Theme>>] {
     const [theme, setTheme] = useState<Theme>(localStorage.theme as Theme || 'light');
-    
-    // The colorTheme is derived from the current theme and is either 'light' or 'dark'
     const colorTheme: Theme = theme === 'dark' ? 'light' : 'dark';
 
-    // Store theme in localStorage whenever it changes
     useEffect(() => {
         const root = window.document.documentElement;
-
-        // Ensure that the root element only has one theme class
         root.classList.remove(colorTheme);
         root.classList.add(theme);
-
         if (localStorage.theme === 'dark') {
             localStorage.removeItem('theme');
         } else {
